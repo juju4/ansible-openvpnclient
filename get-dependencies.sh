331acdd08790e9ede04db53cdfa9fde526eed93e
juju4 <juju4@users.noreply.github.com>
2020-08-02 11:50:22 -0400

    rhel/centos: requires EPEL for openvpn package

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,11 +9,10 @@ else
 rolesdir=$(dirname $0)/..
 fi
 
-#[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat_epel
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.openvpnclient ] && ln -s ansible-openvpnclient $rolesdir/juju4.openvpnclient
 [ ! -e $rolesdir/juju4.openvpnclient ] && cp -R $rolesdir/ansible-openvpnclient $rolesdir/juju4.openvpnclient
 
 ## don't stop build on this script return code
 true
-
